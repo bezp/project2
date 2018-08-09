ed46394355c3746fd75a081c1917a7448ed83a64
bezp <bezalelp@uci.edu>
2018-08-09 16:43:57 -0400

    add the src and test sln w/ projects

---
+++ src/BeckonedPath.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BeckonedPath.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/BeckonedPath.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BeckonedPath.Library
+{
+    public class Class1
+    {
+    }
+}
