c3bce03e54de5b0a33aef86af201bdd4b9015d2a
bezp <bezalelp@uci.edu>
2018-08-10 14:34:24 -0400

    added configuration

--- src/BeckonedPath.Service/Startup.cs
+++ src/BeckonedPath.Service/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -12,25 +13,38 @@ namespace BeckonedPath.Service
 {
     public class Startup
     {
-        public IConfiguration Configuration;
+        public IConfiguration Configuration {get; set;}
+
+        public Startup(IConfiguration Configuration)
+        {
+
+        }
+        //public IConfiguration Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          services.AddMvc();
+          //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder();
+            Configuration = builder.AddJsonFile(@"appsettings.json").Build();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            //app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("hello work");
+            });
 
 
         }
